959037663caef7053a04b43c23b01df9bea5c219
Syed Nayeem Roman <romansyed007@gmail.com>
2020-05-08 16:32:17 +0600

    add axios

--- pokoTime/src/hoc/rainbow.js
+++ pokoTime/src/hoc/rainbow.js
@@ -5,7 +5,11 @@ const Rainbow = (WrappedComponent) =>{
     const randomColor = colors[Math.floor(Math.random()*5)];
     const className = randomColor + '-text';
 
+
+
     return(props) =>{
+        console.log(props)
+
         return(
             <div className={className}>
                 <WrappedComponent {...props} />
